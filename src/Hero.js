c2d0f3aef8d88a5d427d276e5c7728678cbd6315
Truman7777 <79563212+Truman7777@users.noreply.github.com>
2023-01-11 09:23:01 +0900

    landing-page

---
+++ src/Hero.js
@@ -0,0 +1,66 @@
+import {
+  Button,
+  Flex,
+  Heading,
+  Image,
+  Stack,
+  Text,
+  useBreakpointValue,
+} from "@chakra-ui/react";
+
+export default function SplitScreen() {
+  return (
+    <Stack minH={"100vh"} direction={{ base: "column", md: "row" }}>
+      <Flex p={8} flex={1} align={"center"} justify={"center"}>
+        <Stack spacing={6} w={"full"} maxW={"lg"}>
+          <Heading fontSize={{ base: "3xl", md: "4xl", lg: "5xl" }}>
+            <Text
+              as={"span"}
+              position={"relative"}
+              _after={{
+                content: "''",
+                width: "full",
+                height: useBreakpointValue({ base: "20%", md: "30%" }),
+                position: "absolute",
+                bottom: 1,
+                left: 0,
+                bg: "blue.400",
+                zIndex: -1,
+              }}
+            >
+              조율해다오
+            </Text>
+            <br />{" "}
+            <Text color={"blue.400"} as={"span"}>
+              팀프로젝트와 여러분 모두를 위하여
+            </Text>{" "}
+          </Heading>
+          <Text fontSize={{ base: "md", lg: "lg" }} color={"gray.500"}>
+            조율해다오를 사용해주세요.
+          </Text>
+          <Stack direction={{ base: "column", md: "row" }} spacing={4}>
+            <Button
+              rounded={"full"}
+              bg={"blue.400"}
+              color={"white"}
+              _hover={{
+                bg: "blue.500",
+              }}
+            >
+              프로젝트 시작하기
+            </Button>
+          </Stack>
+        </Stack>
+      </Flex>
+      <Flex flex={1}>
+        <Image
+          alt={"Login Image"}
+          objectFit={"cover"}
+          src={"../picture/W3PP.jpg"}
+          // height={"110vh"}
+          // style={{ width: "1000px", height: "1000px" }}
+        />
+      </Flex>
+    </Stack>
+  );
+}
